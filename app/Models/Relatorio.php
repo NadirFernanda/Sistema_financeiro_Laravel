30dd68b25de6e8d15f479a8f8e165110f114c83e
NadirFernanda <fernandadapiedade2018gmail.com>
2026-02-01 00:51:27 +0100

    Fix: corrigir sintaxe duplicada no model Relatorio

--- app/Models/Relatorio.php
+++ app/Models/Relatorio.php
@@ -7,13 +7,4 @@ use Illuminate\Database\Eloquent\Model;
 class Relatorio extends Model
 {
     // Model básico para Relatorio
-}<?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Model;
-
-class Relatorio extends Model
-{
-    // Model básico para Relatorio
 }

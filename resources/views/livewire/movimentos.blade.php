a2364fe02ad1b4bdfad641206fd5c98db97b6b59
NadirFernanda <fernandadapiedade2018gmail.com>
2026-02-02 14:00:57 +0100

    Exibe sempre o campo de fatura em movimentos

--- resources/views/livewire/movimentos.blade.php
+++ resources/views/livewire/movimentos.blade.php
@@ -100,18 +100,15 @@
                 @error('tipo') <span class="text-danger">{{ $message }}</span> @enderror
             </div>
 
-            {{-- Só mostra o campo de fatura quando for Saída --}}
-            @if ($tipo === 'saida')
-                <div class="col-md-3">
-                    <select class="form-select" wire:model.defer="factura_id">
-                        <option value="">Selecione a Fatura (obrigatória para Saída)</option>
-                        @foreach ($facturas as $fatura)
-                            <option value="{{ $fatura->id }}">{{ $fatura->numero_factura }} - {{ $fatura->empresa_nome }}</option>
-                        @endforeach
-                    </select>
-                    @error('factura_id') <span class="text-danger">{{ $message }}</span> @enderror
-                </div>
-            @endif
+            <div class="col-md-3">
+                <select class="form-select" wire:model.defer="factura_id">
+                    <option value="">Selecione a Fatura (obrigatória para Saída)</option>
+                    @foreach ($facturas as $fatura)
+                        <option value="{{ $fatura->id }}">{{ $fatura->numero_factura }} - {{ $fatura->empresa_nome }}</option>
+                    @endforeach
+                </select>
+                @error('factura_id') <span class="text-danger">{{ $message }}</span> @enderror
+            </div>
             <div class="col-md-2">
                 <button type="submit" class="btn btn-primary w-100">
                     {{ $modoEdicao ? 'Atualizar' : 'Cadastrar' }}

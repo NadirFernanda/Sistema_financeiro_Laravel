e1d44e2824e731cfaef9e3d465bfe55fc403bdff
NadirFernanda <fernandadapiedade2018gmail.com>
2026-02-10 15:58:54 +0100

    Test: send password notifications synchronously (remove ShouldQueue)

--- app/Notifications/UsuarioInitialPasswordNotification.php
+++ app/Notifications/UsuarioInitialPasswordNotification.php
@@ -7,7 +7,7 @@ use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class UsuarioInitialPasswordNotification extends Notification implements ShouldQueue
+class UsuarioInitialPasswordNotification extends Notification
 {
     use Queueable;
 
--- app/Notifications/UsuarioResetPasswordNotification.php
+++ app/Notifications/UsuarioResetPasswordNotification.php
@@ -7,7 +7,7 @@ use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class UsuarioResetPasswordNotification extends Notification implements ShouldQueue
+class UsuarioResetPasswordNotification extends Notification
 {
     use Queueable;
 
